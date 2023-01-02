0871059f16e0882ad9e4c3a0a74876cb9200f5d1
Seras3 <adrian_etto@yahoo.com>
2023-01-02 14:07:36 +0200

    pages & routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoPageComponent } from './pages/demo-page/demo-page.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: TestPageComponent },
+  {
+    path: 'demo',
+    component: DemoPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobCardComponent } from './job-card/job-card.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
+import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, JobCardComponent, TestPageComponent, DemoPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
